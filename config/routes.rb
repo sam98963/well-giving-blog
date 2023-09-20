0c90c6eeb015470c51c1c44f5c792f83e970b451
sam98963 <szwilliamson01@gmail.com>
2023-09-20 10:55:22 +0100

    ActionController::RoutingError, trying to solve. Posts can be created in the console

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
+  # config/routes.rb
+  post '/posts', to: 'posts#create'  
+
   devise_for :users
   devise_for :installs
   root 'main#home'
